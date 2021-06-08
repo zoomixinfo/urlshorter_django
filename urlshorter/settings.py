7234e3d49fa211d3d824fb90dd3513d4a77419f2
Sameer Kongath <info@zoomixinfo.in>
2021-06-08 17:14:25 +0530

    Add url

--- urlshorter/settings.py
+++ urlshorter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fx2)@kxh&r_!j-ga*u1zdo^wem&e&z$l_1q_ab+&6e9%8$!m^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','urlshorter-zoomixinfo.herokuapp.com/']
 
 
 # Application definition
